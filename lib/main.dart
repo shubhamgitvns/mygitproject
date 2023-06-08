c366091576cf2dd8dc37bb007c04ce907945fc87
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-06-08 12:26:12 +0530

    Create the tital

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'My Github Project',
     );
   }
 }
